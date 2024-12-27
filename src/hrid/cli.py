62b5df72c2cd8d7ecacb996f83a31586223c8211
Evelyn King <evelyn@evieking.com>
2024-12-26 16:25:50 -0800

    Add a cli for quick tests

---
+++ src/hrid/cli.py
@@ -0,0 +1,18 @@
+"""CLI for hrid."""
+
+import argparse
+
+from hrid import hrid
+
+
+def main():
+    """Define the parse for the hrid CLI."""
+    parser = argparse.ArgumentParser(prog="hrid", description="Generate a human readable id.")
+    parser.add_argument("--seed", default=None)
+    args = parser.parse_args()
+    generated_id = hrid(seed=args.seed)
+    print(generated_id)
+
+
+if __name__ == "__main__":
+    main()
